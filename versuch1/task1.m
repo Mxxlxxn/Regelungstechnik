2d83e5aa46ca6289ec8cd0d8fee2ffe951569eda
Jax Raffnix <75493600+JaxRaffnix@users.noreply.github.com>
2024-03-25 17:21:09 +0100

    worked on task 1

--- versuch1/task1.m
+++ versuch1/task1.m
@@ -1,31 +1,52 @@
 clear
 
-time = linspace(0, 3 * 10^-3, 10^3);  % Hz
+% x-Axis
+time = linspace(0, 3e-3, 10e3);  % seconds
 
-function f1 = myfun(x1, x2, x3, x4)
-    f1 = x1 * sin(2 * pi * x2 * x3 - x4);
+% declare sine function
+function f1 = myfun(amplitude, frequency, time, offset)
+    f1 = amplitude * sin(2 * pi * frequency * time - offset);
 end
 
-f1 = myfun(2, 2* 10^3, time, 0);
-f2 = myfun(4, 6 * 10^3, time, -pi./4);
+
+f1 = myfun(2, 2e3, time, 0);
+f2 = myfun(4, 6e3, time, -pi./4);
 f3 = f1 .* f1;
 
-
+% plotting
 layout = tiledlayout(4,1);
 nexttile
-plot(time, f1, "DisplayName", "x1(t)")
+plot(time, f1)
 xlabel("t in s")
 title("x1(t)")
 nexttile
-plot(time, f2, "DisplayName", "x2(t)")
+plot(time, f2)
 xlabel("t in s")
 title("x2(t)")
 nexttile
-plot(time, f3, "DisplayName", "x2(t)")
+plot(time, f3)
 xlabel("t in s")
-title('x1(t) * x1(t)')
+title('x3(t)')
 nexttile
-plot(f1, f2, "DisplayName", "Liss Figur")
+plot(f1, f2)
 xlabel("x1(t)")
 ylabel("x2(t)")
-title("Liss Figur")
+title("Lissajous-Figur")
+
+% task d
+figure
+t=linspace(0.3,1e3);
+tiledlayout(2,1);
+nexttile
+plot(myfun(2, 2e3, t, 0), myfun(2, 6e3, t, -pi./4))
+xlabel("x1(t)")
+ylabel("x2(t)")
+title("Zeitbereich 0:3 s, 1000 Intervalle")
+
+% task e
+t=linspace(0.3,1e3+1);
+nexttile
+plot(myfun(2, 2e3, t, 0), myfun(2, 6e3, t, -pi./4))
+xlabel("x1(t)")
+ylabel("x2(t)")
+title("Zeitbereich 0:3 s, 1001 Intervalle")
